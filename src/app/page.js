2b619a43f3305e74354a02c699fb3b5ae93a9a6b
Eric-0805 <ericroberts805@gmail.com>
2024-09-24 21:28:33 +0100

    main component n initial css

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header/Header.jsx";
 import Footer from "@/components/Footer/Footer.jsx";
+import Main from "@/components/Main/Main.jsx";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Main />
       <p>METIS CREATED BY BYTE-SIZED FRIENDS</p>
       <Footer />
     </>
