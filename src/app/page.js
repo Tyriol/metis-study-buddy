2693297bf13dba9615586a429f0f7327bb62bb79
4rchie-r <archiereader@outlook.com>
2024-09-24 15:55:06 +0100

    Footer is created and linked. Placehold <p> tag within page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header/Header.jsx";
+import Footer from "@/components/Footer/Footer.jsx";
 
 export default function Home() {
   return (
     <>
-    <Header />
+      <Header />
+      <p>METIS CREATED BY BYTE-SIZED FRIENDS</p>
+      <Footer />
     </>
   );
 }
