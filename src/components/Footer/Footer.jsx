fd182e80062eaed4fe68f8329e3039cc531a2f82
4rchie-r <archiereader@outlook.com>
2024-09-25 13:23:27 +0100

    retry of the failed push

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react';
+import './Footer.css';  
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} METIS.</p>
+      <div className="footer-icon">
+        <p className="material-symbols-outlined">home </p>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </div>
+      <div className="footer-icon">
+        <p className="material-symbols-outlined">chat </p>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </div>
+      <div className="footer-icon">
+        <p className="material-symbols-outlined">user </p>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </div>
     </footer>
   );
 };
