dd771326b02c531e0c0c5ab6da78cb64e1e10c5d
Ryan <ryanshaunsmith@gmail.com>
2024-09-25 17:51:50 +0100

    update linting script

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import './Footer.css';  
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-icon">
         <p className="material-symbols-outlined">home </p>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </div>
       <div className="footer-icon">
         <p className="material-symbols-outlined">chat </p>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </div>
       <div className="footer-icon">
-        <p className="material-symbols-outlined">
-          account_circle
-        </p>
+        <p className="material-symbols-outlined">account_circle</p>
       </div>
     </footer>
   );
