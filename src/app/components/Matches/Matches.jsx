8e7cce664150b92001c0009751c5174213e0f909
Ryan <ryanshaunsmith@gmail.com>
2025-01-23 17:39:49 +0000

    rewrite matches css for mobile view

--- src/app/components/Matches/Matches.jsx
+++ src/app/components/Matches/Matches.jsx
@@ -6,7 +6,7 @@ export default function MatchList({ profile }) {
     <div className={styles.imageTextBox}>
       <div className={styles.imageContainer}>
         <Image
-          className={styles.cardSmall}
+          class={styles.cardSmall}
           src="/assets/images/female-avatar.png"
           width={300}
           height={300}
@@ -15,27 +15,16 @@ export default function MatchList({ profile }) {
       </div>
       <div className={styles.textContainer}>
         <h3>
-          Name: {profile.first_name} {profile.last_name}
+          {profile.first_name} {profile.last_name}
         </h3>
-        <p>A little about me: {profile.about_me}</p>
+        <p>{profile.about_me}</p>
         <p>I want to learn about:</p>
-        <ul>
+        <ul className={styles.subjects}>
           {profile.subjects.map((subject) => {
             return <li key={subject}>{subject}</li>;
           })}
         </ul>
       </div>
-      <p></p>
     </div>
   );
 }
-
-// export default MatchList;
-
-/* Component: List of matches that will pull data from the database and show you the name, avatar, and interests of the other users 
-Having the matches page still accessible from the homepage? Eg not just accessible after clicking on your own profile card. Page would need to reload to update with any new users
-#1 Create React state variable to hold list of matches - useState to manage the state for storing fetched data 
-#2 Make the API request and store the data - data should be fetched after button click of previous page - 'show matches' 
-#3 Render the returned data in a list 
-
-*/
